6a2faa9bc365534f87efbb5f4b7e16ccab88aa95
techkaduna <techkaduna@gmail.com>
2022-10-14 05:40:13 -0700

    Re-submission of project 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,6 @@ int main(void)
 	long long int lli;
 	float f;
 
-	/* print the  size of each data type */
 	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(i));
 	printf("Size of a long int: %lu bytes(s)\n", (unsigned long) sizeof(li));
