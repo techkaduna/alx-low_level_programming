261c99e1566388c395f6817eef69337ef3ac98a3
techkaduna <techkaduna@gmail.com>
2022-10-14 01:55:54 -0700

    Re-submission of project work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* Program entry point */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
