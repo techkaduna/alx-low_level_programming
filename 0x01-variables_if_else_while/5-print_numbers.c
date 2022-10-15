cc70ada7c3bceb01f39baf29efaef9dae22f61ee
techkaduna <techkaduna@gmail.com>
2022-10-14 19:19:14 -0700

    Submission of project 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print a et of numbers between 0-9
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+
+	/*looping through an array of integers*/
+	for (a = 0; a < 10; a++)
+	printf("%d", a);
+	putchar('\n');
+	return (0);
+}
