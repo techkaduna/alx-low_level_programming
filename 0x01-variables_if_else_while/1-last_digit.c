c63a025b2a077bc39989d447ef0a1b7ea81a7750
techkaduna <techkaduna@gmail.com>
2022-10-14 18:02:04 -0700

    Re-submission of project work

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 				}
 				else if ((ld < 6) && (ld != 0))
 				{
-					printf("Last digit of %d id %d and is less than 6 and not 0\n", n, ld);
+					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 				}
 					return (0);
 }
