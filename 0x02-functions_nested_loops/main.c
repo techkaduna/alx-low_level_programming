e038958325e3a2145df4ccb01c6a4976099ca746
techkaduna <techkaduna@gmail.com>
2022-10-19 02:47:01 -0700

    Submission of project 1-alphabet

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -10,3 +10,21 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * print_alphabet - prints all the alphabets in lowercase to the standard output
+ * @c: The alphabets to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	/* For loop to print characters while the exist */
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return;
+}
