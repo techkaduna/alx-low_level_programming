f40c2e47b6c6160131e0b9cc70668b509932fa4e
techkaduna <techkaduna@gmail.com>
2022-10-19 17:38:16 -0700

    Submission of project 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Function prints 10 times lowercase alphabet
+ * @alpha - character to be printed
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i = 0;
+
+	/* while loop to iterate how many times for loop runs */
+	while (1 <= 10)
+	{
+		for (alpha = 'a'; alpha < 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+		i++
+	}
+}
