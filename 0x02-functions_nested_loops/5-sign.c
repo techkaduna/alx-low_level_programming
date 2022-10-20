a29ca9ea70ee2c6472e6a7596cd3aaed1f0a1c0d
techkaduna <techkaduna@gmail.com>
2022-10-19 18:53:21 -0700

    Submission of project 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - function print the sign of an integer
+ * @n: number whose sign is determined
+ *
+ * Return: If positiv1, if negative -1, if zero 0
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
