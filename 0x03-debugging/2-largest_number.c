ae118c0119fb4fd54ebd171caee5aded16dfbbb2
techkaduna <techkaduna@gmail.com>
2022-10-20 14:17:58 -0700

    replace-old-logic

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * largest_number - Returns the largest of three numbers
+ * largest_number - returns the largest of three numbers
  * @a: first number
  * @b: second number
  * @c: third number
@@ -10,19 +10,16 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest = b;
 
-	if ((a > b) && (a > c))
+	if (a > b)
 	{
 		largest = a;
 	}
-	else if ((b > a) && (b > c))
-	{
-		largest = b;
-	}
-	else
+	else if (c > b)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
