1a69f4aaa1a8eeff0a09977eda434a56475a2ac3
techkaduna <techkaduna@gmail.com>
2022-10-20 17:57:50 -0700

    submission of project 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9 and a new line
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+}
