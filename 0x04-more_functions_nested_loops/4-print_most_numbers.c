c122c982358bc10549990554af3753e3583a8bdf
techkaduna <techkaduna@gmail.com>
2022-10-20 18:43:51 -0700

    rewrite-logic-fix-bug

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,14 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		if ((i == 2) || (i == 4))
-			continue;
-		_putchar(i + '0');
-		i++;
+		if ((i != 2) && (i != 4))
+		{
+			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
