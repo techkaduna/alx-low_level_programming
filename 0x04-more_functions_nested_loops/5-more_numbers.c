0b95e3d8bb2068558d31d0688dfe18e90ca546e6
techkaduna <techkaduna@gmail.com>
2022-10-20 19:43:13 -0700

    submission-of-project-6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,17 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	i = 0;
-
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		j = 0;
+		while (j <= 14)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+			j++;
 		}
 		_putchar('\n');
 	}
