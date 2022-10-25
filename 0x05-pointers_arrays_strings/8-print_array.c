de9847bbeaa055ad92059b9a4c3d633e0acfb371
techkaduna <techkaduna@gmail.com>
2022-10-24 20:15:38 -0700

    quic-fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,13 @@
 void print_array(int *a, int n)
 {
 	int i = 0;
+	int j;
 
 	for (i = 0; i < n; i++)
 	{
 		if (i == (n - 1))
-			continue;
+			j = (n - 1);
+			printf(" %d", a[j]);
 		printf("%d, ", a[i]);
 	}
 	printf("\n");
