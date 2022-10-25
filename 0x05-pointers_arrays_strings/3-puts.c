dc5d9054203dc607196dc983a3ce7397731e6221
techkaduna <techkaduna@gmail.com>
2022-10-24 18:41:26 -0700

    fix-lint-and-doc-bug

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _puts - prints string to stdout
- * @s: pointer to string 
- *
+ * _puts - function prints string to stdout takes pointer as argument
+ * @str: pointer to string to be printed to stdout
  * Return: void
  */
 void _puts(char *str)
 {
-	while(*str)
+	while (*str)
 		_putchar(*str++);
 	_putchar('\n');
 }
