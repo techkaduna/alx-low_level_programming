579e35e65b62c9c1baba2299f32ac1c285518c1d
techkaduna <techkaduna@gmail.com>
2022-10-24 19:40:02 -0700

    changed-logic

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,11 @@
  */
 void puts2(char *str)
 {
-	int i;
 	int j;
 
-	while (str[i++])
-		j++;
-	for (i = 0; i < j; i += 2)
-		_putchar(str[i]);
+	j = 0;
+	while (str[j] != '\0')
+		if (j % 2 == 0)
+			_putchar(str[i]);
 	_putchar('\n');
 }
