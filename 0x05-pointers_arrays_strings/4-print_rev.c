99b949ea2504a269956a46792f95bcce5058d759
techkaduna <techkaduna@gmail.com>
2022-10-24 18:51:06 -0700

    submission-of-project-4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reverse of string, takes pointer to char
+ * @s: pointer to string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*s[i++])
+		j++;
+
+	for (i = (j - 1); i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
