a54855202e15864f30ab70bf01349fe51475fbe4
techkaduna <techkaduna@gmail.com>
2023-03-08 17:43:07 -0800

    submit _strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function return the lenght of string
+ * @s: string
+ *
+ * Return: int
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
