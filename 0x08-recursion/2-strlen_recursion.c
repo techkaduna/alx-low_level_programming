28692e385445160d29addab2d1f8cb93d9b5872c
techkaduna <techkaduna@gmail.com>
2023-03-09 02:42:47 -0800

    fix 2-strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,12 +10,13 @@
 
 int _strlen_recursion(char *s)
 {
+	int i = 0;
+
 	if (*s == '\0')
 	{
-		return (0);
+		i++;
+		i += _strlen_recursion(s + 1);
 	}
-	else
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
+
+	return (i);
 }
