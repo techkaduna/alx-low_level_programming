699e8a4d4b5b3200fb40452c6dff9a54620f3afe
techkaduna <techkaduna@gmail.com>
2023-03-22 08:58:19 -0700

    fix-lint-issue

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
-  * array_iterator - executes function given as a parameter on each element of an array.
+  * array_iterator - function given as a parameter on each element of an array.
   * @array: array passed as param
   * @size: size
   * @action: action (function) to execute
